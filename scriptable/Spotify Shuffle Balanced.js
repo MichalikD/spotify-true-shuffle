8dde915922de8e620a141a6c409cdcc2ed970098
MichalikD <David.Michalik@t-online.de>
2026-08-14 12:46:48 +0200

    Update Spotify Shuffle Balanced.js

--- scriptable/Spotify Shuffle Balanced.js
+++ scriptable/Spotify Shuffle Balanced.js
@@ -1,8 +1,5 @@
 // Variables used by Scriptable.
 // These must be at the very top of the file. Do not edit.
-// icon-color: light-brown; icon-glyph: magic;
-// Variables used by Scriptable.
-// These must be at the very top of the file. Do not edit.
 // icon-color: red; icon-glyph: magic;
 function shuffle(
   trackList,
